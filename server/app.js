56ba65b57cef47ae5d238832e65a457e488e75e2
Chaitanya Jawanjal <chaitanyajawanjal20@gmail.com>
2026-02-17 08:40:36 +0530

    changed the name userschma to plantSchema

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const db = require("./db/connect_db");
-const { userSchema } = require("./db/Schema");
+const { plantSchema } = require("./db/Schema");
 const { v4: uuidv4 } = require("uuid");
 const { eq } = require("drizzle-orm");
 
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/get_plats", async (req, res) => {
   try {
-    const result = await db.select().from(userSchema);
+    const result = await db.select().from(plantSchema);
     res.json(result);
   } catch (err) {
     console.log(err);
@@ -24,7 +24,7 @@ app.get("/get_plats", async (req, res) => {
 
 app.post("/create_plant", async (req, res, next) => {
   let id = uuidv4();
-  await db.insert(userSchema).values({
+  await db.insert(plantSchema).values({
     id,
     name: req.body.name,
     des: req.body.des,
@@ -40,13 +40,13 @@ app.put("/update_plant/:id", async (req, res) => {
     const { id } = req.params;
 
     await db
-      .update(userSchema)
+      .update(plantSchema)
       .set({
         name: req.body.name,
         des: req.body.des,
         code: req.body.code,
       })
-      .where(eq(userSchema.id, id));
+      .where(eq(plantSchema.id, id));
 
     res.send("Plant updated successfully");
   } catch (err) {
@@ -59,7 +59,7 @@ app.delete("/delete_plant/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
-    await db.delete(userSchema).where(eq(userSchema.id, id));
+    await db.delete(userSchema).where(eq(plantSchema.id, id));
 
     res.send("Plant deleted successfully");
   } catch (err) {
